8f705f17506b15abaa468742ee47e1a89de19fb7
danielrojas08 <41838381+danielrojas08@users.noreply.github.com>
2020-03-10 12:36:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import os
+def searchGoogle():
+  os.system('cd skills')
+  import searchGoogle
 
+def searchYoutube():
+  os.system('cd skills')
+  import searchYoutube
+  
+print("Enter command in command prompt")
+print('Write help for list of commands')
+print('Contact me at quicktypeninja@gmail.com')
+prompt = input('>>> ')
+if prompt == 'search google' 'Search Google' 'Search google':
+  searchGoogle()
+if command == 'search youtube' 'Search Youtube' "Search youtube":
+  searchYoutube()
+if command != 'search youtube' 'Search Youtube' "Search youtube" 'search google' 'Search Google' 'Search google':
+  print('That is not a command')
+  print('You can make it one at https://github.com/danielrojas08/MagiAI/')
